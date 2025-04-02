8ac6ba3def493449476290fcd464105936c00976
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-04-01 21:45:59 -0500

    task 4 complete

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,6 @@
-import dbClient from '../utils/db.js'
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 
 class UsersController {
@@ -32,6 +34,31 @@ class UsersController {
 
         // Return response with user ID and email
         return res.status(201).json({ id: result.insertedId, email });
+    }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        try {
+            const key = `auth_${token}`;
+            const userId = await redisClient.get(key);
+            if (!userId) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+            const user = await dbClient.db.collection('users').findOne(
+                { _id: new ObjectId(userId) },
+                { projection: {email: 1 } }
+            );
+            if (!user) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+            return res.status(200).json({ id: user._id, email: user.email });
+        } catch (err) {
+            console.error('Error in getMe:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
     }
 }
 
